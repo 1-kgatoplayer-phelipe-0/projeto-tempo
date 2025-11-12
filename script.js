bb9dc84e9277e1ed10d565607cac2a77aa54188d
1-kgatoplayer-phelipe-0 <victorlealphelipe@gmail.com>
2025-11-12 20:25:43 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-document.quarySelector('.busca').addEventListener("submit",async (event)=>{
-    event.preventDefaut();
-    let input = document.querySelector("#searchInput").value;
-    if(input !=='')
+document.querySelector('.busca').addEventListener('submit', async (event)=>{
+    event.preventDefault();
+    let input = document.querySelector('#searchInput').value;
+    if(input !== '')
     {
         clearInfo();
-        showWaring('carregando...')
-        let url= `https://api.opewathermap.org/data/2.5/weather?q=${encode(input)}&appid=ef60a79c9c3ca99f2edfad01fd9badb3&units=metrica&lag=pt_br`
+        showWarning('Carregando...')
+        let url= `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=ef60a79c9c3ca99f2edfad01fd9badb3&units=metric&lang=pt_br`
 
         let results = await fetch(url);
         let json = await results.json();
@@ -14,66 +14,66 @@ document.quarySelector('.busca').addEventListener("submit",async (event)=>{
             showInfo({
                 name: json.name,
                 country: json.sys.country,
-                temp: json.weather[0].icon,
-                tempIcon: json.wind.speed,
-                WindSpeed: json.wind.speed,
-                descri : json.weather[0].description,
+                temp: json.main.temp,
+                tempIcon: json.weather[0].icon,
+                windSpeed: json.wind.speed,
+                 desc: json.weather[0].description,
             }
-            )
-         }
-        else{
+            );
+        } 
+        else {
             clearInfo();
-            showWaring('Não encontramos essa localização');
-         }   
+            showWarning('Não encontramos essa localização');
+        }
     }else{
-    clearInfo();
-}
+        clearInfo();
+    }
 })
 function showInfo(json){
-    showWaring("");
-    document.quarySelector('.resultado').style.display= 'block';
-    document.quarySelector('.titulo').innerHTML=`${json.name}, ${json.country}`
-    document.quarySelector('.temperatura').innerHTML = `${json.temp} <sup>ºC</sup>`;
-    document.querySelector('.ventoInfo').innerHTML = `${json.WindSpeed} <span>km/h</span>`;
-    document.quarySelector('.tempInfo').innerHTML= `${json.descri}`;
-    document.quarySelector('.informacoes img').setAttribute('src',`./img/${json.tempIcon}.gif`);
+    showWarning('');
+    document.querySelector('.resultado').style.display = 'block';
+    document.querySelector('.titulo').innerHTML = `${json.name}, ${json.country}`;
+    document.querySelector('.temperatura').innerHTML = `${json.temp} <sup>ºC</sup>`;
+    document.querySelector('.ventoinfo').innerHTML = `${json.windSpeed} <span>km/h</span>`;
+    document.querySelector('.tempinfo').innerHTML = `${json.desc}`;
+    document.querySelector('.informacoes img').setAttribute('src', `./img/${json.tempIcon}.gif`)
 }
-function showWaring(msg)
+    function showWarning(msg)
 {
-    document.quarySelector('.aviso').innerHTML = msg
+    document.querySelector('.aviso').innerHTML = msg;
 }
 function clearInfo(){
-    showWaring('');
-    document.quarySelector('.resultado').style.display = 'none'
+    showWarning('')
+    document.querySelector('.result').style.display = 'none';
 }
-async function curitiba (){
-    let Input = 'curitiba'
-    if (input !=='')
-    {  
+ async function curitiba(){
+    let input = 'Curitiba'
+    if(input !=='')
+    {
         clearInfo();
-        showWaring('carregando...')
+        showWarning('Carregando...');
 
-        let url= `https://api.openwathermap.org/data/2.5/wather?q=${encodeURI(input)}&appid=ef60a79ca99f2edfad01fd9badb3&units=metric&lang=pr_br`
+        let url= `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=ef60a79c9c3ca99f2edfad01fd9badb3&units=metric&lang=pt_br`
         let results = await fetch(url);
         let json = await results.json();
-        if(json.cod === 200){
+        if(json.cod === 200) {
             showInfo({
-            name: json.name,
-            coutry: json.sys.coutry,
-            temp: json.main.temp,
-            tempIcon: json.weather[0].icon,
-            WindSpeed: json.wind.speed,
-            descri : json.weather[0].description,
+                name: json.name,
+                country: json.sys.country,
+                temp: json.main.temp,
+                tempIcon: json.weather[0].icon,
+                windSpeed: json.wind.speed,
+                descri : json.weather[0].description,
             }
             );
         }
         else{
             clearInfo();
-            showWaring('não encontramos essa localização')
+            showWarning('Não encontramos sua localização');
         }
     }else{
         clearInfo();
     }
-}
+ }
 
-curitiba();
+ curitiba();
